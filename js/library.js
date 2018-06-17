eb09834e0735d2b12e29497246daf128b1985a07
Nova Yin <laciepark300323@hotmail.com>
2018-06-17 19:11:48 +0200

    Contrast Function
    Added contrast function where the colro of the text is changed when the bg color is lighter or darker

--- js/library.js
+++ js/library.js
@@ -76,31 +76,67 @@ function $(selector)
 		return self
 	}
 
-	/*self.contr = function(name, value)
+	self.noHover = function(name, value)
+	{
+		self.element.setAttribute("style", "color: white;");
+		return self;
+	
+	}
+
+	//Changes color when hovered over.
+	self.hover = function(name, value)
+	{
+		if (!value) self.element.setAttribute("style", "color: green;");
+		self.element.setAttribute(name, value);
+		return self;
+	
+	}
+
+	//Changees Contrast when background is lighther or darker
+	//This code uses a part of the randomcol function above 
+	self.contr = function(name, value)
 	{
 		var rgb = ['255', '0', '0']; //Default rbg property list
 
+		//return rgb[0]; <--- use to check if list items work
+
+		setInterval(function()
+		{
+
+		//Stores the random generated rgb color code into bgcol variable by using the ['255', '0', '0'] given above.
+		var bgcol = 'rgb('+rgb[0]+','+rgb[1]+','+rgb[2]+')';
+		//return bgcol; <--- use to check if the rgb code generating works
+
+		//This is an algorythm I found on 
+		//http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx 
+		//To calculate the brightness of a color
+		var contrast = Math.floor(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587)+(parseInt(rgb[0]) * 114))/1000);
+		//return contrast; //<--- use to check if getting the contrast value works
+		
+
+		//Sets the rgb value from bgcol into the css style background-color
+		//And changes the bg color every second (1000), change the 1000 to make it slower or faster.
+		self.element.setAttribute("style", "background-color:" +bgcol);
+
+		//If the color's brightness is above 125 (A bright color according to the algorythm) it will change the text color to black
+		//If the color's brightness is below 125 (A dark color according to the algorythm) it will change the text color to white
+		//The bgcol here is to change the background color in intervals
+		if (contrast > 125)
+		{
+			self.element.setAttribute("style", "color: black; background-color:" +bgcol);
+		}
+		else
+		{
+			self.element.setAttribute("style", "color: white; background-color:" +bgcol);
+		}
+		
 		rgb[0] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color red
 		rgb[1] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color green
 		rgb[2] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color blue
 		
-		//return rgb[0]; <--- use to check if list items work
 
-		//This is an algoritme I found on 
-		//http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx 
-		//To calculate the brightness of a color
-		var contrast = Math.floor((parseInt(rgb[0] * 299) + (parseInt(rgb[1] * 587))+(parseInt(rgb[0] * 114))/1000));
-		//return contrast; //<--- use to check if getting the contrast value works
-		
-		if (contrast > 125)
-		{
-			self.element.setAttribute("style", "color: black;");
-		}
-		else
-		{
-			self.element.setAttribute("style", "color: white;");
-		}
-		return self; 
-	}*/
+		}, 1000);
+		return self;
+	}
 	return self
 }
