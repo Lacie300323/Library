b212e0063d98119a003489763e4d531605ab9ccb
Nova Yin <laciepark300323@hotmail.com>
2018-06-17 17:06:42 +0200

    Added randomcol and contrast functions
    Added 2 new functions. Randomcolor for random bg colors in intervals and contrast to change text by contrast

--- js/library.js
+++ js/library.js
@@ -50,5 +50,52 @@ function $(selector)
 		return self;
 	}
 
+	self.randomcol = function (name, value)
+	{
+		var rgb = ['255', '0', '0']; //Default rbg property list consisting of 3 numbers between 0 and 255
+
+
+		setInterval(function()
+		{
+
+		rgb[0] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color red
+		rgb[1] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color green
+		rgb[2] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color blue
+
+		var bgcol = 'rgb('+rgb[0]+','+rgb[1]+','+rgb[2]+')';
+		//return bgcol; <--- use to check if the rgb code generating works
+
+		//Sets the rgb value from bgcol into the css style background-color
+		self.element.setAttribute("style", "background-color:" +bgcol);
+		}, 1000);
+		return self
+	}
+
+	/*self.contr = function(name, value)
+	{
+		var rgb = ['255', '0', '0']; //Default rbg property list
+
+		rgb[0] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color red
+		rgb[1] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color green
+		rgb[2] = Math.floor(Math.random() * 255); //Returns a random number between 0 and 255 for the color blue
+		
+		//return rgb[0]; <--- use to check if list items work
+
+		//This is an algoritme I found on 
+		//http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx 
+		//To calculate the brightness of a color
+		var contrast = Math.floor((parseInt(rgb[0] * 299) + (parseInt(rgb[1] * 587))+(parseInt(rgb[0] * 114))/1000));
+		//return contrast; //<--- use to check if getting the contrast value works
+		
+		if (contrast > 125)
+		{
+			self.element.setAttribute("style", "color: black;");
+		}
+		else
+		{
+			self.element.setAttribute("style", "color: white;");
+		}
+		return self; 
+	}*/
 	return self
 }
